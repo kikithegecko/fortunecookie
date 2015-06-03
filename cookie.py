57aba3db3f1463e617d81a195321b7abc02d6297
Karoline Busse <busse@cs.uni-bonn.de>
2015-06-03 19:58:18 +0200

    now with proper line breaking!

--- cookie.py
+++ cookie.py
@@ -4,6 +4,28 @@ import subprocess
 fortune = ''
 while (fortune == '') or (len(fortune) > 140):
 	fortune = subprocess.getoutput('fortune')
-	fortune.replace('\t', ' ')
-	print("Your fortune is:")
+	fortune = fortune.replace('\t', ' ')
+	fortune = fortune.replace('\n', ' ')
+#print("Your fortune is:")
+#print(fortune)
+
+# step 2: insert newlines, so that line width is at most 28 chars.
+lines = []
+for i in range(5):
+	pos = 28
+	try: # check if the remaining fortune is shorter than 28 chars
+		fortune[pos]
+	except:
+		lines.append(fortune)
+		fortune = ''
+		break
+	while fortune[pos] != ' ':
+		pos -= 1
+	#fortune[pos] = '' # so that the new line doesn't start with a space
+	lines.append(fortune[:pos].lstrip())
+	fortune = fortune[pos:].lstrip()
+#for i in range(len(lines)):
+#	print(lines[i])
+if len(fortune) > 0:
+	print("PANIC! Fortune turned out to be too long. This is the rest:")
 	print(fortune)
